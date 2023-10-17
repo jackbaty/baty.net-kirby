eb6ca3d87af2d638a614d1ee37a62a4e59c58160
Jack Baty <jack@baty.net>
2023-10-17 10:57:47 -0400

    Add search

---
+++ site/templates/search.php
@@ -0,0 +1,18 @@
+<?php snippet('header') ?>
+
+<form id="search-form">
+  <input type="search" class="search-field" aria-label="Search" name="q" value="<?= html($query) ?>" autofocus>
+  <input type="submit" class="search-button" value="Search">
+</form>
+
+<ul class="search-results">
+  <?php foreach ($results as $result): ?>
+  <li>
+    <a href="<?= $result->url() ?>">
+      <?= $result->title() ?>
+    </a>
+  </li>
+  <?php endforeach ?>
+</ul>
+
+<?php snippet('footer') ?>
