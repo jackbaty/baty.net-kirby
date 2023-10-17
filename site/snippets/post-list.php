35f71cba3790beb878a04cdf5a1bd062785e86fa
Jack Baty <jack@baty.net>
2023-10-17 10:59:10 -0400

    Show tags on post list

--- site/snippets/post-list.php
+++ site/snippets/post-list.php
@@ -11,7 +11,7 @@
 ?>
 <article class="post-excerpt">
 <h2 class="post-excerpt-title"><a href="<?= $post->url() ?>"><?= $post->title()->esc() ?></a></h2>
-<time class="post-excerpt-date" datetime="<?= $post->published('c') ?>"><?= $post->published() ?></time>
+<time class="post-excerpt-date" datetime="<?= $post->published('c') ?>"><?= $post->published() ?><?php if (!empty($post->tags())): ?> | Tags: <?= $post->tags() ?><?php endif ?></time> 
     <?php if (($excerpt ?? true) !== false): ?>
     <div class="post-excerpt-text">
       <?php if ($post->summary()->isEmpty()): ?>
