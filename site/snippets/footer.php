2bc74ba0c813c322b111d942856890c53e0e3235
Jack Baty <jack@baty.net>
2023-10-18 07:43:40 -0400

    Remove everything but Copyright from footer
    It was messy

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -18,22 +18,7 @@
          Copyright © 2000-2023 Jack Baty
         </p>
       </div>
-      <div class="column" style="--columns: 2">
-        <h2>Pages</h2>
-        <ul>
-          <?php foreach ($site->children()->listed() as $example): ?>
-          <li><a href="<?= $example->url() ?>"><?= $example->title()->esc() ?></a></li>
-          <?php endforeach ?>
-        </ul>
-      </div>
-      <div class="column" style="--columns: 2">
-        <h2>Elsewhere</h2>
-        <ul>
-          <li><a href="https://wiki.baty.net">Wiki</a></li>
-          <li><a href="https://social.lol/@jbaty">Mastodon</a></li>
-          <li><a href="https://flickr.com/photos/jbaty">Flickr</a></li>
-        </ul>
-      </div>
+      
     </div>
   </footer>
 
