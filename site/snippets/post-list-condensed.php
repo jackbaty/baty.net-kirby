4ece26f0d961b2d32cba54ac146e4363668391d4
Jack Baty <jack@baty.net>
2024-02-05 11:15:01 -0500

    Publish checkpoint

--- site/snippets/post-list-condensed.php
+++ site/snippets/post-list-condensed.php
@@ -9,16 +9,4 @@
 	
 		<?= $post->published() ?>
 		<?php if (!empty($post->tags())): ?> | <?= $post->tags() ?><?php endif ?>
-
-
-    <?php if (($excerpt ?? true) !== false): ?>
-    <p class="post-excerpt-text">
-      <?php if ($post->summary()->isEmpty()): ?>
-      	<?= $post->text()->kt()->excerpt(80) ?>
-      <?php else: ?>
-      	<?= $post->summary()->text() ?>
-      <?php endif ?>
-  
-    </p>
-    <?php endif ?>
 </ul>
